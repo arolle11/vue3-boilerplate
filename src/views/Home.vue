be96537bd33345b1f08a33e90dc71157f66ac5f2
Arolle11 <arollefona11@gmail.com>
2025-05-29 21:34:16 +0100

    add tailwind css and vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptas
+    praesentium atque quo harum accusamus, officiis molestiae esse temporibus
+    blanditiis magnam aperiam eaque officia autem, inventore laboriosam
+    accusantium voluptatem laudantium!
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
