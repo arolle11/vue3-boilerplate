41c8f1ee5ebb522e4d6867170041ca123e40c4a1
Arolle11 <arollefona11@gmail.com>
2025-06-04 23:40:06 +0100

    navigation and side bar structure

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="p-6 z-40 md:border-b md:border-[#C8CBD9]">
-    <div class="container mx-auto flex justify-between items-center">
+  <header class="md:border-b md:border-[#C8CBD9] shadow p-6">
+    <div class="container flex justify-between items-center">
       <button
         @click="uiStore.toggleSidebar"
         class="md:hidden p-2 focus:outline-none text-[#C8CBD9] text-3xl"
@@ -8,19 +8,46 @@
         ☰
       </button>
 
-      <nav class="hidden md:block">
-        <div class="flex items-center justify-between">
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. molestias,
-            blanditiis eum aliquid dignissimos fuga eveniet nesciunt nam cumque!
-            Doloribus, distinctio excepturi
+      <nav class="hidden md:block w-full">
+        <div class="flex items-center justify-between gap-12">
+          <div class="flex flex-1">
+            <form class="w-full">
+              <label
+                for="default-search"
+                class="mb-2 text-sm font-medium text-gray-900 sr-only"
+                >Search</label
+              >
+              <div class="relative">
+                <div
+                  class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
+                >
+                  <Search class="w-4 h-4 text-[#1F384C]" />
+                </div>
+                <input
+                  type="search"
+                  id="default-search"
+                  class="block w-full p-4 ps-10 text-sm text-[#1F384C] rounded-lg bg-[#F1F2F7] outline-none"
+                  placeholder="Search..."
+                  required
+                />
+              </div>
+            </form>
           </div>
-          <div class="flex items-center">
+
+          <div class="flex items-center gap-4">
             <div
               class="p-2 bg-orange-200 flex items-center justify-center rounded-full text-xs"
             >
               <Hamburger class="w-6 h-6" />
             </div>
+
+            <button
+              class="text-[#273240] rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+              type="button"
+            >
+              Delicious Burger
+              <ChevronDown class="w-4 h-4" />
+            </button>
             <BellDot class="w-6 h-6" />
           </div>
         </div>
@@ -30,6 +57,6 @@
 </template>
 <script setup lang="ts">
 import { useUIStore } from "@/stores/ui";
-import { BellDot, Hamburger } from "lucide-vue-next";
+import { BellDot, Hamburger, ChevronDown, Search } from "lucide-vue-next";
 const uiStore = useUIStore();
 </script>
--- src/components/layout/AppSidebar.vue
+++ src/components/layout/AppSidebar.vue
@@ -1,89 +1,87 @@
 <template>
   <aside
-    class="bg-[#F1F2F7] text-black w-64 h-full md:static fixed top-0 left-0 transition-all duration-300 z-50"
+    class="bg-[#F1F2F7] text-black w-64 h-full md:static fixed top-0 left-0 transition-all duration-300 z-50 p-6"
     :class="{
       '-translate-x-full md:translate-x-0': !uiStore.isSidebarOpen,
       'translate-x-0': uiStore.isSidebarOpen,
     }"
   >
-    <div class="flex items-center gap-2 p-6 border-b border-[#d9c8d3]">
+    <div class="flex items-center gap-2">
       <div
-        class="text-white bg-[#5A67BA] flex items-center justify-center w-6 h-6 rounded-full text-xs"
+        class="text-white bg-[#5A67BA] flex items-center justify-center w-8 h-8 rounded-full text-xs"
       >
         G
       </div>
-      <h1 class="text-xs font-bold text-[#5A67BA]">Good Food</h1>
+      <h1 class="text-xs font-bold text-[#5A67BA]">GOODFOOD</h1>
     </div>
 
-    <div class="p-6">
-      <nav @click="closeSidebar" class="space-y-6">
-        <ul class="space-y-4 mt-6 text-[#273240]">
-          <span class="">MENU</span>
-          <li class="mt-4">
-            <RouterLink
-              to="/"
-              class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
-            >
-              <LayoutDashboard class="w-6 h-6" />Dashboard</RouterLink
-            >
-          </li>
-          <li>
-            <RouterLink
-              to="/"
-              class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
-            >
-              <ShoppingCart class="w-6 h-6" />Food Order</RouterLink
-            >
-          </li>
-          <li>
-            <RouterLink
-              to="/"
-              class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
-              ><BookText class="w-6 h-6" />Manage Menu</RouterLink
-            >
-          </li>
-          <li>
-            <RouterLink
-              to="/"
-              class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
-              ><MessageCircleMore class="w-6 h-6" />Customer Review</RouterLink
-            >
-          </li>
-        </ul>
-        <ul class="space-y-4 text-[#273240]">
-          <span></span>
-          OTHERS
-          <li class="mt-4">
-            <RouterLink
-              to="/"
-              class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
-              ><Settings class="w-6 h-6" /> Settings</RouterLink
-            >
-          </li>
-          <li>
-            <RouterLink
-              to="/"
-              class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
-              ><CreditCard class="w-6 h-6" />Payment</RouterLink
-            >
-          </li>
-          <li>
-            <RouterLink
-              to="/"
-              class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
-              ><User class="w-6 h-6" /> Accounts</RouterLink
-            >
-          </li>
-          <li>
-            <RouterLink
-              to="/"
-              class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
-              ><Info class="w-6 h-6" />Help</RouterLink
-            >
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav @click="closeSidebar" class="space-y-6">
+      <ul class="space-y-4 mt-6 text-[#273240]">
+        <span class="">MENU</span>
+        <li class="mt-4">
+          <RouterLink
+            to="/"
+            class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
+          >
+            <LayoutDashboard class="w-6 h-6" />Dashboard</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            to="/"
+            class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
+          >
+            <ShoppingCart class="w-6 h-6" />Food Order</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            to="/"
+            class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
+            ><BookText class="w-6 h-6" />Manage Menu</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            to="/"
+            class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
+            ><MessageCircleMore class="w-6 h-6" />Customer Review</RouterLink
+          >
+        </li>
+      </ul>
+      <ul class="space-y-4 text-[#273240]">
+        <span></span>
+        OTHERS
+        <li class="mt-4">
+          <RouterLink
+            to="/"
+            class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
+            ><Settings class="w-6 h-6" /> Settings</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            to="/"
+            class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
+            ><CreditCard class="w-6 h-6" />Payment</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            to="/"
+            class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
+            ><User class="w-6 h-6" /> Accounts</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            to="/"
+            class="flex gap-2 hover:bg-[#e3e7f4] hover:text-[#707FDD] p-2"
+            ><Info class="w-6 h-6" />Help</RouterLink
+          >
+        </li>
+      </ul>
+    </nav>
   </aside>
 </template>
 
